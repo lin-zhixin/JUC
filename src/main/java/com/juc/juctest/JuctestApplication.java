6dd421b12ae0f2a64b0f9df86195d953f07134b7
lin-zhixin <386856805@qq.com>
2023-04-02 13:12:34 +0800

    add init

---
+++ src/main/java/com/juc/juctest/JuctestApplication.java
@@ -0,0 +1,13 @@
+package com.juc.juctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuctestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuctestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/juc/juctest/JuctestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juc.juctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuctestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
